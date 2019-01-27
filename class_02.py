0f827cb4be4baa136b557736c0cf0a57840b855e
abhishek4554 <barge9599abhi@gmail.com>
2019-01-27 12:54:13 +0530

    data types & data structures

---
+++ class_02.py
@@ -0,0 +1,10 @@
+# my name is abhishek. all are comments
+print("hie") #ijko
+"""
+file = module in python
+"""
+'''
+data type  int,float,string,boolean,none- says particular datatype as it doe not have value actually false
+data structure - list, dictionary.
+'''
+# everything in python is call by reference, what is done after delete a file
