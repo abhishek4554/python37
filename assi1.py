1639c55cbcacf46ef30591cae8b4ddbde1e649d8
abhishek4554 <barge9599abhi@gmail.com>
2019-02-01 10:19:13 +0530

    assingment 1

---
+++ assi1.py
@@ -0,0 +1,67 @@
+
+print("Assignment 01")
+# integers
+print("Integer Variables")
+integer_variable_01 = 'aa'
+aa = 10
+print(aa)
+integer_variable_02 = 'bb'
+bb = 20
+print(bb)
+integer_variable_03 = 'cc'
+cc = 30
+print(cc)
+integer_variable_04 = 'dd'
+dd = 40
+print(dd)
+int_variable_05 = 'ee'
+ee = 50
+print(ee)
+
+# float
+
+print("Float variables")
+float_variable_1 = 'a'
+a = 4.5
+print(a)
+flt_variable_2 = 'b'
+b = 0.9
+print(b)
+flt_variable_3 = 'c'
+c = 0.0054
+print(c)
+flt_variable_4 = 'd'
+d = 4565.545
+print(d)
+flt_variable_5 = 'e'
+e = 868.42
+print(e)
+
+# string
+
+str_variable = 'There are two types of string'
+str_variable_2 = "1st is single line string" \
+                 "2nd is multi line string"
+str_variable_3 = """ string is a datatype to store characters in it.
+                      it is widely used datatype
+                         it helps in reducing efforts"""
+str_variable_4 = 'hi'
+
+str_variable_5 = " i am abhishek"
+
+print(str_variable)
+print(str_variable_2)
+print(str_variable_3)
+print(str_variable_4)
+print(str_variable_5)
+
+# Boolean
+print("Boolean variables")
+true_flag = a
+a = 'yes'
+print(a)
+false_flag = b
+b = 'no'
+print(b)
+
+
