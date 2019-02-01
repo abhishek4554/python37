b7295264fba05139e76d40eb978fe0e9accbf2b6
abhishek4554 <barge9599abhi@gmail.com>
2019-02-01 17:22:00 +0530

    assignment 2

---
+++ Assi2.py
@@ -0,0 +1,15 @@
+print("Assingmemt 2")
+# list variables
+list_variable_01 = ["Karan ", "pune", 9503745654]
+list_variable_02 = ["Aarohi", "Bombay", 9637286787]
+list_variable_03 = ["Aaryan", "Chennai", 9172945424]
+list_variable_04 = ["Riya", "Pawai", 9172032676]
+list_variable_05 = ["kapil", "Punjab", 9404018667]
+print(list_variable_01)
+print(list_variable_02)
+print(list_variable_03)
+print(list_variable_04)
+print(list_variable_05)
+
+
+
